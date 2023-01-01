00c7782f754705145cc55644a3cdcf596cc41b4c
= <=>
2023-01-01 18:24:46 -0300

    Adicionando views e menu

---
+++ src/views/home.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Home() {
+    return (
+        <>
+            <div className="jumbotron">
+                <h1 className="display-3">Bem vindo!</h1>
+                <p className="lead">
+                    Este é seu sistema, utilize a barra para navegar entre as paginas!
+                </p>
+                <hr className="my-4" />
+                <p>
+
+                </p>
+                <p className="lead">
+                    <a className="btn btn-primary btn-lg" href="#" role="button">Cadastrar</a>
+                </p>
+            </div>
+        </>
+    )
+}
+
+export default Home;
