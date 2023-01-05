ab2a8049a8107c60ddb53d212776d02f86d84131
= <=>
2023-01-05 20:33:00 -0300

    criando state e botoes submit e limpar

--- src/views/produtos/cadastro.js
+++ src/views/produtos/cadastro.js
@@ -1,6 +1,38 @@
 import React from "react";
 
+const estadoInicial = {
+    nome: '',
+    sku: '',
+    descricao: '',
+    preco: 0,
+    fornecedor: ''
+
+}
+
 export default class CadastroProduto extends React.Component {
+
+    state = estadoInicial
+
+
+    onChange = (event) => {
+        const valor = event.target.value
+        const nomeCampo = event.target.name
+
+        this.setState({
+            [nomeCampo]: valor
+        })
+
+    }
+
+    onSubmit = (event) => {
+        console.log(this.state)
+        this.setState(estadoInicial)
+    }
+
+    limparCampos = () => {
+        this.setState(estadoInicial)
+    }
+
     render() {
         return (
             <div className="card">
@@ -12,13 +44,13 @@ export default class CadastroProduto extends React.Component {
                         <div className="col-md-6">
                             <div className="form-group">
                                 <label>Nome: *</label>
-                                <input type="text" className="form-control" />
+                                <input type="text" name="nome" className="form-control" onChange={this.onChange} value={this.state.nome} />
                             </div>
                         </div>
                         <div className="col-md-6">
                             <div className="form-group">
                                 <label>SKU: *</label>
-                                <input type="text" className="form-control" />
+                                <input type="text" name="sku" className="form-control" onChange={this.onChange} value={this.state.sku} />
                             </div>
                         </div>
                     </div>
@@ -26,7 +58,7 @@ export default class CadastroProduto extends React.Component {
                         <div className="col-md-12">
                             <div className="form-group">
                                 <label>Descrição</label>
-                                <input className="form-control" />
+                                <input className="form-control" name="descricao" onChange={this.onChange} value={this.state.descricao} />
                             </div>
                         </div>
                     </div>
@@ -34,22 +66,22 @@ export default class CadastroProduto extends React.Component {
                         <div className="col-md-6">
                             <div className="form-group">
                                 <label>Preço: *</label>
-                                <input type="text" className="form-control" />
+                                <input type="text" className="form-control" onChange={this.onChange} name="preco" value={this.state.preco} />
                             </div>
                         </div>
                         <div className="col-md-6">
                             <div className="form-group">
                                 <label>Fornecedor: *</label>
-                                <input type="text" className="form-control" />
+                                <input type="text" className="form-control" onChange={this.onChange} name="fornecedor" value={this.state.fornecedor} />
                             </div>
                         </div>
                     </div>
                     <div className="row">
                         <div className="col-md-1">
-                            <button className="btn btn-success">Salvar</button>
+                            <button onClick={this.onSubmit} className="btn btn-success">Salvar</button>
                         </div>
                         <div className="col-md-1">
-                            <button className="btn btn-primary">Limpar</button>
+                            <button className="btn btn-primary" onClick={this.limparCampos}>Limpar</button>
                         </div>
                     </div>
 
